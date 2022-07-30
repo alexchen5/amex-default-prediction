41733224df90b144ed3bbd1381c5404b805bfebe
alex <4lexchen5@gmail.com>
2022-07-30 23:14:02 +1000

    usage for subsample

--- src/subsample_data.py
+++ src/subsample_data.py
@@ -8,19 +8,39 @@ The results can then be used for data preprocessing.
 import random 
 import pandas as pd
 
-n_samples = 100
+# x_in: feature set
+# y_in: label set related to features, or None
+# n_samples: number of CIDs to select
+def subsample(x_in: str, x_out: str, y_in=None, y_out=None, n_samples=100):
+    df = pd.read_parquet(x_in)
+    
+    cid = df['customer_ID'].unique()
+    cid_sample = cid[random.sample(range(cid.size), n_samples)]
+    
+    df = df[df['customer_ID'].isin(cid_sample)]
+    df.to_parquet(x_out, index=False)
+    print(df.shape)
+    
+    if y_in:
+        labels = pd.read_csv(y_in)
+        labels = labels[labels['customer_ID'].isin(cid_sample)]
+        labels.to_csv(y_out, index=False)
+        print(labels.shape)
 
-df = pd.read_parquet(f'../input/amex-data-integer-dtypes-parquet-format/train.parquet')
-labels = pd.read_csv(f'../input/amex-default-prediction/train_labels.csv')
+if __name__ == "__main__":
+    dir_in = '../input/amex-data-integer-dtypes-parquet-format'
+    dir_out = '../input/subsampled'
 
-cid = df['customer_ID'].unique()
-cid_sample = cid[random.sample(range(cid.size), n_samples)]
-
-df = df[df['customer_ID'].isin(cid_sample)]
-labels = labels[labels['customer_ID'].isin(cid_sample)]
-
-df.to_parquet(f'../input/subsampled/train.parquet', index=False)
-labels.to_csv(f'../input/subsampled/train_labels.csv', index=False)
-
-print(df.shape)
-print(labels.shape)
+    y_train = '../input/amex-default-prediction/train_labels.csv'
+    
+    subsample(
+        x_in=f'{dir_in}/train.parquet', x_out=f'{dir_out}/train.parquet',
+        y_in=y_train, y_out=f'{dir_out}/train_labels.csv',
+        n_samples=1000
+    )
+    
+    # No related labels to subsample
+    subsample(
+        x_in=f'{dir_in}/test.parquet', x_out=f'{dir_out}/test.parquet',
+        n_samples=2000
+    )
