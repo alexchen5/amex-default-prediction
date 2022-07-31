109c30c3e2d62d06f580fd1b7f5c7ccb7c34f913
alex <4lexchen5@gmail.com>
2022-08-01 01:20:31 +1000

    add printing

--- src/predictCb.py
+++ src/predictCb.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import catboost as cb
+from amex_metric import amex_metric
+import numpy as np
 
-test = pd.read_parquet(f'../test_input/shrunk_test.parquet')
+# test = pd.read_parquet(f'../test_input/shrunk_test.parquet')
+test = pd.read_parquet(f'../input/processed/train.parquet')
+test_labels = pd.read_csv(f'../input/amex-default-prediction/train_labels.csv').target.values
+
 cb = cb.CatBoostClassifier()
 cb.load_model("../models/cb_model.json")
-print(cb.predict_proba(test)[:,1])
+pred = cb.predict_proba(test)[:,1]
+
+print(np.mean(amex_metric(test_labels, pred)))
--- src/predictLgb.py
+++ src/predictLgb.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import lightgbm as lgb
+from amex_metric import amex_metric
+import numpy as np
 
 # test = pd.read_parquet(f'../test_input/shrunk_test.parquet')
-x_test = pd.read_parquet(f'../input/processed/test.parquet')
+
+test = pd.read_parquet(f'../input/processed/train.parquet')
+test_labels = pd.read_csv(f'../input/amex-default-prediction/train_labels.csv').target.values
+# test = pd.read_parquet(f'../input/processed/test.parquet')
 
 lg = lgb.Booster(model_file="../models/lgb_model.json")
-# print(lg.predict(test))
-pred = lg.predict(x_test)
+pred = lg.predict(test)
 
-sub = pd.DataFrame({'customer_ID': x_test.index,
-                        'prediction': pred})
-sub.to_csv('../output/submission_lgbm.csv', index=False)
+print(np.mean(amex_metric(test_labels, pred)))
+
+# sub = pd.DataFrame({'customer_ID': x_test.index,
+#                         'prediction': pred})
+# sub.to_csv('../output/submission_lgbm.csv', index=False)
